342ac251b386f45ce83d3072f038a0397f03a9c0
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-03-07 18:25:12 +0100

    page title updated

--- src/web/src/views/About.js
+++ src/web/src/views/About.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Layout} from "./Layout";
+import Helmet from "react-helmet";
 
 export function About() {
-    return(
-        <Layout>about view</Layout>
+    return (
+        <React.Fragment>
+            <Helmet>
+                <title>About author</title>
+            </Helmet>
+            <Layout>about view</Layout>
+        </React.Fragment>
     )
 }
--- src/web/src/views/Projects.js
+++ src/web/src/views/Projects.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import {Layout} from "./Layout";
+import {Helmet} from "react-helmet";
 
 export function Projects() {
-    return(
-        <Layout>projects view</Layout>
+    return (
+        <React.Fragment>
+            <Helmet>
+                <title>My projects</title>
+            </Helmet>
+            <Layout>projects view</Layout>
+        </React.Fragment>
     )
 }
