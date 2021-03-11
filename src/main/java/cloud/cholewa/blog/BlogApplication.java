a02c2b85fb48d8de1c96ed055d10853cda41fbc7
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-03-11 12:00:22 +0100

    fixed problem with starting class

--- src/main/java/cloud/cholewa/blog/BlogApplication.java
+++ src/main/java/cloud/cholewa/blog/BlogApplication.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class CourseApplication {
+public class BlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CourseApplication.class, args);
+        SpringApplication.run(BlogApplication.class, args);
     }
 }
